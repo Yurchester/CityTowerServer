d87590b15251d8f65abbc38f1f6eb4af714ee88a
Petr Lozhchenko <peetlucifer@gmail.com>
2018-06-23 20:27:40 +0300

    Creating server, DAL, API

---
+++ CityTowerWebAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CityTowerWebAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
